f37930a9bd17f1b7b77ed6588c5ec1c0f6688d82
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-09-06 08:55:34 +0600

    added tailwind

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const getInputText = (id) => {
     const inputField = document.getElementById(id)
     const inputText = inputField.value 
+    inputField.value = ''
     return inputText
 }
-document.getElementById('')
+document.getElementById('add-btn').addEventListener('click', () => {
+    const productName = getInputText('name-field')
+    const productQuantity = getInputText('quantity-field')
+    console.log(productName, productQuantity)
+    localStorage.setItem(productName,productQuantity)
+})
