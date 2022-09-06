03082616b60ab6cb90cab62597d588e91d87e1a1
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-09-06 10:03:08 +0600

    table adding function addeded

--- app.js
+++ app.js
@@ -8,5 +8,14 @@ document.getElementById('add-btn').addEventListener('click', () => {
     const productName = getInputText('name-field')
     const productQuantity = getInputText('quantity-field')
     console.log(productName, productQuantity)
+    // product showing container
+    const productContainer = document.getElementById('show-product')
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+    <th></th>
+     <th>${productName}</th>
+     <th>${productQuantity}</th>
+    `
+    productContainer.appendChild(tr)
     localStorage.setItem(productName,productQuantity)
 })
